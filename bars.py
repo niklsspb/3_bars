692bd3cb41af2f5e19f6d23ca1aaec1bd4dffda9
niklsspb <prof051@yandex.ru>
2016-12-15 15:55:38 +0300

    Update bars.py
    fix and add name

--- bars.py
+++ bars.py
@@ -19,13 +19,13 @@ def load_data(filepath):
 def get_biggest_bar(data):
     data = load_data('data.json')
     i = 0
-    minim = data[i]['SeatsCount']
     maxim = data[i]['SeatsCount']
     while i < len(data):
         if data[i]['SeatsCount']>maxim:
             maxim = data[i]['SeatsCount']
+            name_bar = data[i]['Name']
         i += 1
-    print(maxim)
+    print(name_bar)
 
 
 def get_smallest_bar(data):
@@ -35,25 +35,36 @@ def get_smallest_bar(data):
     while i < len(data):
         if data[i]['SeatsCount']<minim:
             minim = data[i]['SeatsCount']
+            name_bar = data[i]['Name']
         i += 1
-    print(minim)
+    print(name_bar)
 
 
 def get_closest_bar(data, longitude, latitude):
-    data = load_data('data.json')
+    data = load_data(data)
+    i = 0
+    ids = 0
+    min_euql = 50
+    while i < len(data):
+        a = euqlid(float(data[i]['Longitude_WGS84']), float(data[i]['Latitude_WGS84']), longitude, latitude)
+        if a < min_euql:
+            min_euql = a
+            ids = data[i]['global_id']
+            name_bar = data[i]['Name']
+        i += 1
+    print(name_bar)
 
 
-def euqlid(longitude, latitude):
-    x1 = float(input())
-    y1 = float(input())
-    d = math.sqrt(math.pow((longitude-x1),2)+math.pow((latitude-y1),2))
-    print(d)
+def euqlid(longitude, latitude, x1, y1):
+    d = math.sqrt(math.pow((longitude-x1), 2)+math.pow((latitude-y1), 2))
+    return d
 
 
-long = 37.6215879462381080
-lat = 55.7653669567739740
-euqlid(long, lat)
-# get_biggest_bar('data.json')
-# get_smallest_bar('data.json')
+long = float(input("Введите долготу "))
+lat = float(input("Введите ширину "))
+
+get_biggest_bar('data.json')
+get_smallest_bar('data.json')
+get_closest_bar('data.json', long, lat)
 if __name__ == '__main__':
     pass
