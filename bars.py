c37c8df4ed217719ff1a050488e66939a7265eed
niklsspb <prof051@yandex.ru>
2017-01-26 19:41:55 +0300

    Fix блок try...except
    Уменьши тело try...except в ifmain., уменьшил и сделал проверку на пустоту
    У многих функций проблема с отступами: 8 вместо 4. лишние отступы убраны.

--- bars.py
+++ bars.py
@@ -11,38 +11,41 @@ def load_data(path_to_file):
 
 
 def get_biggest_bar(data):
-        big_bar = max(data, key=lambda k: k['SeatsCount'])
-        return big_bar['Name']
+    big_bar = max(data, key=lambda k: k['SeatsCount'])
+    return big_bar['Name']
 
 
 def get_smallest_bar(data):
-        min_bar = min(data, key=lambda k: k['SeatsCount'])
-        return min_bar['Name']
+    min_bar = min(data, key=lambda k: k['SeatsCount'])
+    return min_bar['Name']
 
 
 def get_closest_bar(data, longitude, latitude):
-        for bar in data:
-            bar['distance_key'] = calculate_the_euclidean_distance(float(bar['Longitude_WGS84']),
+    for bar in data:
+        bar['distance_key'] = calculate_the_euclidean_distance(float(bar['Longitude_WGS84']),
                                                                  float(bar['Latitude_WGS84'])
                                                                  , longitude, latitude)
-        closest_bar = min(data, key=lambda k: k['distance_key'])
-        return closest_bar['Name']
+    closest_bar = min(data, key=lambda k: k['distance_key'])
+    return closest_bar['Name']
 
 
 def calculate_the_euclidean_distance(longitude_json, latitude_json, input_user_longitude, input_user_latitude):
-        distance = (((longitude_json-input_user_longitude)**2)+((latitude_json-input_user_latitude)**2))**0.5
-        return distance
+    distance = (((longitude_json-input_user_longitude)**2)+((latitude_json-input_user_latitude)**2))**0.5
+    return distance
 
 
 if __name__ == '__main__':
-    try:
-        input_file_name = input("Введите имя файла(путь к файлу) ")
-        latitude = input('Введите latitude : ')
-        longitude = input("Введите longitude ")
-        data_input = load_data(input_file_name)
-        print("Самый большой бар = {0} \n"
-              "Самый маленький бар = {1} \n"
-              "Самый близкий бар = {2}".format(get_biggest_bar(data_input), get_smallest_bar(data_input),
+    input_file_name = input("Введите имя файла(путь к файлу) ")
+    latitude = input('Введите latitude : ')
+    longitude = input("Введите longitude ")
+    data_input = load_data(input_file_name)
+    if data_input is None:
+        print("В указанном файле нету данных или он отсутствует")
+    else:
+        try:
+            print("Самый большой бар = {0} \n"
+                "Самый маленький бар = {1} \n"
+                "Самый близкий бар = {2}".format(get_biggest_bar(data_input), get_smallest_bar(data_input),
                                                get_closest_bar(data_input, float(longitude), float(latitude))))
-    except ValueError:
-        print ("Исходные данные были указаны не верны. Значения latitude и longitude только числа")
+        except ValueError:
+            print ("Исходные данные были указаны не верны. Значения latitude и longitude только числа")
